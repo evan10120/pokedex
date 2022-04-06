a5a851650228f780d45d240a107aa8182ad9673d
Evan <evan010@hotmail.fr>
2022-04-06 08:12:25 +0200

    Modification et stylisation de la barre de navigation

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import PokemonDetail from "../Screens/PokemonDetail";
+import {Image, View} from "react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -23,13 +24,57 @@ export default function Navigation(){
     return(
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen options={{title: "Pokedex", headerTintColor: "white", headerStyle: {backgroundColor: "red"} }}
+                <Tab.Screen options={{title: "Pokedex", headerTintColor: "white", headerStyle: {backgroundColor: "red"}, tabBarIcon: ({focused}) => (
+                    <View>
+                        <Image source={require('../assets/pokeball.png')}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                        }}
+
+                        />
+                    </View>
+                    ) }}
                             name="Home"
                             component={PokemonStack}
                             />
-                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"} }} name="Research" component={PokemonStack} />
-                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"} }} name="Teams" component={PokemonStack} />
-                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"} }} name="Profils" component={PokemonStack} />
+                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"}, tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image source={require('../assets/research.png')}
+                                   resizeMode="contain"
+                                   style={{
+                                       width: 25,
+                                       height: 25,
+                                   }}
+
+                            />
+                        </View>
+                    ) }} name="Research" component={PokemonStack} />
+                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"}, tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image source={require('../assets/teams.png')}
+                                   resizeMode="contain"
+                                   style={{
+                                       width: 25,
+                                       height: 25,
+                                   }}
+
+                            />
+                        </View>
+                    ) }} name="Teams" component={PokemonStack} />
+                <Tab.Screen options={{headerTintColor: "white", headerStyle: {backgroundColor: "red"}, tabBarIcon: ({focused}) => (
+                        <View>
+                            <Image source={require('../assets/profile.webp')}
+                                   resizeMode="contain"
+                                   style={{
+                                       width: 25,
+                                       height: 25,
+                                   }}
+
+                            />
+                        </View>
+                    ) }} name="Profils" component={PokemonStack}  />
             </Tab.Navigator>
         </NavigationContainer>
     )
